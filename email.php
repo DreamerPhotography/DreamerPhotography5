ab122154d0c243835a6d54064ba0b6f87d0a217f
DreamerPhotography <64855050+DreamerPhotography@users.noreply.github.com>
2022-01-31 08:50:07 +0000

    Update email.php

--- email.php
+++ email.php
@@ -1,6 +1,20 @@
 <?php
 
-echo "hello world"
+    $email = $_POST['email'];
 
+    //Database connection
+    $conn = new mysqli('localhost', 'root','','test');
+    if($conn->connect_error){
+      die('Connection Failed : '.$conn->connect_error);
+    }else{
+      $stmt = $conn->prepare("insert into registraion(email) 
+       values(?)"){
+      $stmt->bind_param("s",$email);
+      $stmt->execute();
+      echo ""registration Successfully...";
+      $stmt->close();
+      $conn->close();
+    }
+    
 
 ?>
